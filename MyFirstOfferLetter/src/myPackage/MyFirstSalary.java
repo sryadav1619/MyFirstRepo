2505f19848855283959045804bfd07145367a346
sryadav1619 <yadavsantosh1619@gmail.com>
2023-04-09 18:25:15 -0700

    FirstCommitProcess
    I completed my task successfully. Please review it so I can merge it......

--- MyFirstOfferLetter/src/myPackage/MyFirstSalary.java
+++ MyFirstOfferLetter/src/myPackage/MyFirstSalary.java
@@ -4,8 +4,8 @@ public class MyFirstSalary {
 
 	public static void main(String[] args) {
 		
-		System.out.println("My First Package is 11 LPA !!!!!!!!!!!!!!!!");
-
+		System.out.println("My First Package is 5 LPA !!!!!!!!!!!!!!!!");
+        System.out.println("I am very Happy on that day when my first salary is credited on my account");
 	}
 
 }
